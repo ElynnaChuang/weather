f5ea51840e51907d582e8d866e1eb6052d802b94
yichennn <yichennn1010@gmail.com>
2023-05-19 00:44:41 +0800

    Fixed: CityInfo page style & Cities list style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.scss';
 import { Route, Routes, HashRouter } from 'react-router-dom';
+import { useEffect, useRef } from 'react';
 import { routes } from './config/routes';
 
 const App = () => {
+  const appRef = useRef();
+  useEffect(() => {
+    appRef.current.style.setProperty('--vh', `${window.innerHeight}px`);
+    window.addEventListener('resize', () =>
+      appRef.current.style.setProperty('--vh', `${window.innerHeight}px`),
+    );
+  }, []);
+
   return (
-    <div className='app'>
+    <div className='app' ref={appRef}>
       <HashRouter basename='/'>
         <Routes>
           {routes.map(({ path, element }) => (
--- src/utils/getWeatherIcon.jsx
+++ src/utils/getWeatherIcon.jsx
@@ -1,6 +1,6 @@
 import { weatherIcons } from '@/assets/weather_icons';
 
-export const getWeatherIcon = (weather = 'clear', hour = 12) => {
+export const getWeatherIcon = (weather = 'cloudy', hour = 12) => {
   let icon;
   if (hour >= 6 && hour < 18) {
     icon = weatherIcons.light[weather];
