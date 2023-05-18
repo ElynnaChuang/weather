5b2bfe6f3a2313f1a9c1cf8ff82c51c814ea1d60
yichennn <yichennn1010@gmail.com>
2023-05-19 01:01:17 +0800

    Adjust: bg from svg component to css background image property & weather icon file adjust

--- src/layouts/Center/index.jsx
+++ src/layouts/Center/index.jsx
@@ -1,11 +1,8 @@
 import styles from './styles.module.scss';
 
-import { ReactComponent as BackgroundImage } from '@/assets/bg.svg';
-
 export const CenterLayout = ({ children }) => {
   return (
     <div className={styles.page}>
-      <BackgroundImage />
       <section className={styles.section}>
         <div className={styles.container}>{children}</div>
       </section>
