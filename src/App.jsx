9b21b43d98832c49b2b1d6f1f0529f28f6d87a18
yichennn <yichennn1010@gmail.com>
2023-05-18 17:30:02 +0800

    Fixed style & router reload error

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import './App.scss';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 import { routes } from './config/routes';
 
 const App = () => {
   return (
     <div className='app'>
-      <BrowserRouter basename='/weather'>
+      <HashRouter basename='/'>
         <Routes>
           {routes.map(({ path, element }) => (
             <Route key={path} path={path} element={element} />
           ))}
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 };
